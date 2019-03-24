3f8ef5f778c778a52cb4be18b5448066e4a7391f
songjein <jeinsong100@gmail.com>
2019-03-24 15:43:17 +0000

    request 한글 url

--- server.js
+++ server.js
@@ -2,19 +2,21 @@
 // sharp tutorial: https://malcoded.com/posts/nodejs-image-resize-express-sharp
 // sharp width request: https://github.com/lovell/sharp/issues/247
 
+const path = require('path');
+const sharp = require('sharp');
 const express = require('express');
 const request = require('request');
+const urlencode = require('urlencode');
 
-const sharp = require('sharp');
+const app = express();
 
-const server = express();
+//app.use(express.json({limit: '50mb'}));
+//app.use(express.urlencoded({limit: '50mb', extended: true}));
 
-server.get('/', (req, res) => {
+app.get('/resize', (req, res) => {
 	const widthStr = req.query.width;
 	const heightStr = req.query.height;
-	const imgUrl = req.query.url;
-
-	console.log(widthStr, heightStr, imgUrl);
+	let imgUrl = req.query.url;
 
 	let width, height;
 	if (widthStr) {
@@ -24,6 +26,12 @@ server.get('/', (req, res) => {
 		height = parseInt(heightStr);	
 	}
 
+	const split = imgUrl.split('/');
+	const path = split.slice(0, split.length-1).join('/');
+	const filename = split.slice(-1)[0];
+
+	imgUrl = path + '/' + urlencode(filename);
+
 	request({url: imgUrl, encoding: null}, function(err, response, buffer) {
 		sharp(buffer)
 			.resize(width, height)
@@ -31,7 +39,12 @@ server.get('/', (req, res) => {
 	});
 });
 
+
+app.get('/test', (req, res) => {
+	res.json({status: 200});
+});
+
 //ex: http://stdio.kr:3000/?url=https://~~~~~~~~~~~~~~/uploads/photo/image/21790/1.png&width=300
-server.listen(3000, () => {
+app.listen(6666, () => {
 	console.log('Server started');
 });
